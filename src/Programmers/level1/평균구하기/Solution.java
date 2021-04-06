563f95a09033fd39bfd994545373b8e2b960acfa
m3k0813 <m3k0813@gmail.com>
2021-04-06 19:09:02 +0900

    algorithm

---
+++ src/Programmers/level1/평균구하기/Solution.java
@@ -0,0 +1,13 @@
+package Programmers.level1.평균구하기;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double sum=0;
+        for(int i=0; i<arr.length;i++){
+            sum += arr[i];
+        }
+        answer = sum/arr.length;
+        return answer;
+    }
+}
