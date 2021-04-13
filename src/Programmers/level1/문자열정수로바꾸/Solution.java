317f590a24f472227fba71fa108075215907a34e
m3k0813 <m3k0813@gmail.com>
2021-04-13 13:57:53 +0900

    algorithm

---
+++ src/Programmers/level1/문자열정수로바꾸/Solution.java
@@ -0,0 +1,9 @@
+package Programmers.level1.문자열정수로바꾸;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.valueOf(s);
+        return answer;
+    }
+}
