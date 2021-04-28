d9756775a6d55a813b82afe52a7415a3edec15a1
m3k0813 <m3k0813@gmail.com>
2021-04-28 16:16:39 +0900

    algorithm

---
+++ src/Programmers/level1/콜라츠추측/Solution.java
@@ -0,0 +1,24 @@
+package Programmers.level1.콜라츠추측;
+
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+
+        while(num!=1){
+
+            if(num%2==0){
+                num = num/2;
+            }else{
+                num = num*3+1;
+            }
+            answer++;
+
+            if(answer>=500){
+                answer=-1;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
